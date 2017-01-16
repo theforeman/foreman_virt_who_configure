42c7c5146d141adc18d5c21f4e75f53593c35c58
Marek Hulan <mhulan@redhat.com>
2017-01-16 12:54:22 +0100

    Don't conflict with other plugin wizards

--- app/helpers/foreman_virt_who_configure/configs_helper.rb
+++ app/helpers/foreman_virt_who_configure/configs_helper.rb
@@ -7,7 +7,7 @@ module ForemanVirtWhoConfigure
           text    = overwrite ? overwrite : _("Submit")
           options = {:class => "btn btn-primary"}
           options.merge! :'data-id' => form_to_submit_id(form) unless options.key?(:'data-id')
-          previous = form.object.first_step? ? ' ' : previous_link(form)
+          previous = form.object.first_step? ? ' ' : previous_config_link(form)
           cancel_and_submit = content_tag(:div, :class => "pull-right") do
             link_to(_("Cancel"), args[:cancel_path], :class => "btn btn-default") + ' ' + form.submit(text, options)
           end
@@ -16,11 +16,11 @@ module ForemanVirtWhoConfigure
       end
     end
 
-    def show_partial_wizard(step)
+    def show_config_partial_wizard(step)
       @config.current_step == step ? '' : 'hidden'
     end
 
-    def previous_link(form)
+    def previous_config_link(form)
       previous = content_tag(:span, :class => 'glyphicon glyphicon-chevron-left') {}
       content_tag(:div, :class => 'pull-left') do
         link_to((previous).html_safe, '#', :class => 'btn btn-default', :onclick => "previous_step('#{@config.previous_step}')")
