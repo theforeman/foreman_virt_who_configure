7081a6b9ac8fc068b277cf940a7c3d2fa35d08f8
Marek Hulan <mhulan@redhat.com>
2017-02-24 16:52:31 +0100

    Fixes #18668 - restore compatibility with recent Foreman

--- app/helpers/foreman_virt_who_configure/compatibility_helper.rb
+++ app/helpers/foreman_virt_who_configure/compatibility_helper.rb
@@ -3,16 +3,17 @@ module ForemanVirtWhoConfigure
     extend ActiveSupport::Concern
 
     def new_config_link(options = {}, html_options = {})
-      if respond_to?(:new_link)
-        new_link(_("New Config"), options, html_options)
+      if ApplicationHelper.instance_methods.include?(:new_link)
+        new_link(_("Create Config"), options, html_options)
       else
         display_link_if_authorized(_("New Config"), hash_for_new_foreman_virt_who_configure_config_path, html_options)
       end
     end
 
     def help_button_or_path
-      if respond_to?(:help_button)
-        help_button
+      if ApplicationHelper.instance_methods.include?(:help_button)
+        # help_button does not support plugins
+        link_to(_("Help"), { :action => "welcome" }, { :class => 'btn btn-default' })
       else
         help_path
       end
