29a524a1349b1b0f0b8bb2083cafad1ff323ea75
Marek Hulán <ares@users.noreply.github.com>
2017-04-25 14:36:38 +0200

    Fixes #18989 - disable service user login

--- app/models/foreman_virt_who_configure/auth_source_hidden_with_authentication.rb
+++ app/models/foreman_virt_who_configure/auth_source_hidden_with_authentication.rb
@@ -6,8 +6,17 @@ module ForemanVirtWhoConfigure
       end
     end
 
+    def auth_method_name
+      "HIDDEN with API access"
+    end
+    alias_method :to_label, :auth_method_name
+
     def authenticate(login, password)
-      AuthSourceInternal.new.authenticate(login, password)
+      # the authentication should only happen if our SSO signals that it's available
+      # we need to reset the signal since for non API calls the SSO is not used at all and does not set signal to false
+      result = Thread.current[:service_user_login] ? AuthSourceInternal.new.authenticate(login, password) : false
+      Thread.current[:service_user_login] = nil
+      result
     end
   end
 end
