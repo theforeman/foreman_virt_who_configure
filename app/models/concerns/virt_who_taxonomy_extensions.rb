3b90e5442575bd444b1246bf98885d04b34cf71f
Marek Hulan <mhulan@redhat.com>
2018-11-22 18:01:02 +0100

    Refs #23878 - fix the deletion completely

--- app/models/concerns/virt_who_taxonomy_extensions.rb
+++ app/models/concerns/virt_who_taxonomy_extensions.rb
@@ -1,6 +1,13 @@
 module VirtWhoTaxonomyExtensions
   extend ActiveSupport::Concern
   included do
-    has_one :config, :dependent => :destroy, :class_name => 'ForemanVirtWhoConfigure::Config'
+    # the order is imporant here, around filter needs to disable audit creation, otherwise config deletion
+    # creates audit during org deletion and creates foreign key issue, the relation must be defined later
+    around_destroy :ignore_auditing_of_config
+    has_many :configs, :dependent => :destroy, :class_name => 'ForemanVirtWhoConfigure::Config'
+
+    def ignore_auditing_of_config
+      ::ForemanVirtWhoConfigure::Config.without_auditing { yield }
+    end
   end
 end
