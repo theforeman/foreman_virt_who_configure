505b2c218c5182658b4ba69cf994e7ec0f28c932
Marek Hulán <ares@users.noreply.github.com>
2019-07-31 09:02:23 +0200

    Fixes #27438 - add welcome route (#106)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :foreman_virt_who_configure do
+    get 'configs/help', :action => :welcome, :controller => 'configs'
     resources :configs do
       collection do
         get 'auto_complete_search'
