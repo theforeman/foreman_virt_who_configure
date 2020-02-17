882a2b1d55b58d5062db57e85582281d2cd0b440
Jonathon Turel <jturel@gmail.com>
2020-02-17 21:26:12 +0000

    Fix factories

--- test/factories/foreman_virt_who_configure_factories.rb
+++ test/factories/foreman_virt_who_configure_factories.rb
@@ -2,23 +2,23 @@ FactoryBot.define do
   factory :virt_who_config, :class => ::ForemanVirtWhoConfigure::Config do
     sequence(:name) { |n| "config #{n}" }
     organization
-    interval 120
-    hypervisor_id 'hostname'
-    hypervisor_type 'esx'
-    hypervisor_server 'vmware.example.com'
-    hypervisor_username 'root'
-    hypervisor_password 'changeme'
-    satellite_url 'foreman.example.com'
-    listing_mode ForemanVirtWhoConfigure::Config::UNLIMITED
+    interval { 120 }
+    hypervisor_id { 'hostname' }
+    hypervisor_type { 'esx' }
+    hypervisor_server { 'vmware.example.com' }
+    hypervisor_username { 'root' }
+    hypervisor_password { 'changeme' }
+    satellite_url { 'foreman.example.com' }
+    listing_mode { ForemanVirtWhoConfigure::Config::UNLIMITED }
   end
 
   trait :out_of_date do
-    last_report_at (1.minute.ago - 120.minutes).utc
-    out_of_date_at (1.minute.ago).utc
+    last_report_at { (1.minute.ago - 120.minutes).utc }
+    out_of_date_at { (1.minute.ago).utc }
   end
 
   trait :ok do
-    last_report_at (1.minute.ago).utc
-    out_of_date_at (1.minute.ago + 120.minutes).utc
+    last_report_at { (1.minute.ago).utc }
+    out_of_date_at { (1.minute.ago + 120.minutes).utc }
   end
 end
