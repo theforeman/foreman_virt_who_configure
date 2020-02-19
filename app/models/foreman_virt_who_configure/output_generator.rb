1d9f23830fb43b5b48e69ca1e0c3082b137af813
Jonathon Turel <jturel@gmail.com>
2020-02-19 04:07:51 -0500

    Fixes #29057 - Hosts filtering missing from kubevirt configs (#117)

--- app/models/foreman_virt_who_configure/output_generator.rb
+++ app/models/foreman_virt_who_configure/output_generator.rb
@@ -99,7 +99,7 @@ if verify_minimal_version; then
 type=#{type}
 hypervisor_id=#{hypervisor_id}
 owner=#{owner}
-env=Library#{connection_details}
+env=Library#{connection_details}#{filtering}
 rhsm_hostname=#{satellite_url}
 rhsm_username=#{service_user_username}
 rhsm_encrypted_password=$user_password
@@ -142,7 +142,7 @@ EOS
       else
         "\nserver=#{cr_server}
 username=#{cr_username}
-encrypted_password=$cr_password#{filtering}"
+encrypted_password=$cr_password"
       end
     end
 
