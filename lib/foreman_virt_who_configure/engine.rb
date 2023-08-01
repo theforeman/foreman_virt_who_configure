92eb989091745b996e6034f5719fc15421099af5
Lucy Fu <lufu@redhat.com>
2023-08-01 18:11:09 +0000

    Bump version to 0.5.16 with pulling from tx

--- lib/foreman_virt_who_configure/engine.rb
+++ lib/foreman_virt_who_configure/engine.rb
@@ -28,6 +28,7 @@ module ForemanVirtWhoConfigure
     initializer 'foreman_virt_who_configure.register_plugin', :before => :finisher_hook do |_app|
       Foreman::Plugin.register :foreman_virt_who_configure do
         requires_foreman '>= 3.7'
+        register_gettext
 
         apipie_documented_controllers ["#{ForemanVirtWhoConfigure::Engine.root}/app/controllers/foreman_virt_who_configure/api/v2/*.rb"]
 
