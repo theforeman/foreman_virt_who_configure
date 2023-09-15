theforeman/foreman_virt_who_configure

commit 2ae0ab0629c59b753d16beb563e7a3fbd5690dd1

Bump version to 0.5.16 with pulling from tx (#169)

lib/foreman_virt_who_configure/engine.rb
@@ -28,6 +28,7 @@ module ForemanVirtWhoConfigure
     initializer 'foreman_virt_who_configure.register_plugin', :before => :finisher_hook do |_app|
       Foreman::Plugin.register :foreman_virt_who_configure do
         requires_foreman '>= 3.7'
+        register_gettext
 
         apipie_documented_controllers ["#{ForemanVirtWhoConfigure::Engine.root}/app/controllers/foreman_virt_who_configure/api/v2/*.rb"]
 

commit f213964c064276f8c51e4c3998b87ecfa6dacd7b

Fix translations infrastructure
When using register_gettext, the initializer should be removed since
it'll end up with a duplicate registration. It also failed to update the
Makefile so it didn't update the JS locale files. This causes HTTP 404
errors at runtime and missing translations when webpack is used.

Fixes: 2ae0ab0629c59b753d16beb563e7a3fbd5690dd1 ("Bump version to 0.5.16 with pulling from tx (#169)")

.tx/config
@@ -7,5 +7,5 @@ source_file = locale/foreman_virt_who_configure.pot
 source_lang = en
 type        = PO
 
-minimum_perc  = 0
+minimum_perc  = 50
 resource_name = foreman_virt_who_configure

lib/foreman_virt_who_configure/engine.rb
@@ -111,12 +111,6 @@ module ForemanVirtWhoConfigure
         ForemanVirtWhoConfigure::Engine.load_seed
       end
     end
-
-    initializer 'foreman_virt_who_configure.register_gettext', after: :load_config_initializers do |_app|
-      locale_dir = File.join(File.expand_path('../../..', __FILE__), 'locale')
-      locale_domain = 'foreman_virt_who_configure'
-      Foreman::Gettext::Support.add_text_domain locale_domain, locale_dir
-    end
   end
 
   def self.with_katello?

locale/Makefile
@@ -6,14 +6,15 @@
 # make tx-update - download and merge translations from Transifex
 # make clean - clean everything
 #
-DOMAIN = foreman_virt_who_configure
-VERSION = $(shell ruby -e 'require "rubygems";spec = Gem::Specification::load(Dir.glob("../*.gemspec")[0]);puts spec.version')
+DOMAIN = $(shell ruby -rrubygems -e 'puts Gem::Specification::load(Dir.glob("../*.gemspec")[0]).name')
+VERSION = $(shell ruby -rrubygems -e 'puts Gem::Specification::load(Dir.glob("../*.gemspec")[0]).version')
 POTFILE = $(DOMAIN).pot
 MOFILE = $(DOMAIN).mo
 POFILES = $(shell find . -name '$(DOMAIN).po')
 MOFILES = $(patsubst %.po,%.mo,$(POFILES))
 POXFILES = $(patsubst %.po,%.pox,$(POFILES))
 EDITFILES = $(patsubst %.po,%.edit.po,$(POFILES))
+JSFILES = $(shell find ../app/assets/javascripts/*/locale -name '$(DOMAIN).js')
 
 %.mo: %.po
 	mkdir -p $(shell dirname $@)/LC_MESSAGES
@@ -42,18 +43,22 @@ uniq-po:
 	done
 
 tx-pull: $(EDITFILES)
-	tx pull -f
+	cd .. && tx pull -f
 	for f in $(EDITFILES) ; do \
 		sed -i 's/^\("Project-Id-Version: \).*$$/\1$(DOMAIN) $(VERSION)\\n"/' $$f; \
 	done
 
 tx-update: tx-pull
 	@echo
-	@echo Run rake plugin:gettext[$(DOMAIN)] from the Foreman installation, then make -C locale mo-files to finish
+	@echo Run rake plugin:gettext[$(DOMAIN)] from the Foreman installation
+	@echo then run rake plugin:po_to_json[$(DOMAIN)] from the Foreman installation
+	@echo then run make -C locale mo-files to finish
 	@echo
 
-mo-files: $(MOFILES)
-	git add $(POFILES) $(POTFILE) ../locale/*/LC_MESSAGES
+build-mo-files: $(MOFILES)
+
+mo-files: build-mo-files
+	git add $(POFILES) $(POTFILE) $(JSFILES) ../locale/*/LC_MESSAGES
 	git commit -m "i18n - pulling from tx"
 	@echo
 	@echo Changes commited!